d480ca98be6e0d9140cbab9007a99d76185ef8f7
ParkGeongi <pgg113579@naver.com>
2022-11-10 09:39:40 +0900

    2022/11/10

---
+++ data_structure/unit_5_skill.py
@@ -0,0 +1,4 @@
+street_distance = 12
+print(int(0.2467*street_distance+4.159))
+AP = 102
+print(AP*0.6+225)
---
+++ data_structure/unit_6_average.py
@@ -0,0 +1,4 @@
+a,b,c = map(int,input('변수 : ').split())
+print(a+b+c)
+x,y,z = map(str,input('입력 : ').split())
+print(f'{x}\n{y}\n{z}')
